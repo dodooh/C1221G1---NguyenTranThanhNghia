0fe2d3228b14daa1739e7889fa7252e623cf59f4
Nguyen <nguyen2697s@gmail.com>
2022-01-04 16:55:16 +0700

    add bt ss7 ung dung may tinh

---
+++ ss7-bien_kieu_du_lieu_va_toan_tu/bai_tap/UngDungMayTinh.js
@@ -0,0 +1,8 @@
+function doCalc(e) {
+    let a = parseFloat(document.getElementById("num1").value);
+    let b = parseFloat(document.getElementById("num2").value);
+    let operate = e.target.value;
+    let result = eval(a + operate + b);
+    document.getElementById("show-result").innerText = result;
+}
+
