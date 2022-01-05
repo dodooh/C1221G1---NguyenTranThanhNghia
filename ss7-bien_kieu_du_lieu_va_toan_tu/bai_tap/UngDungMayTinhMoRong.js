0b926d832204978d6ec8a2c60f0f0784d9ed0327
Nguyen <nguyen2697s@gmail.com>
2022-01-05 14:24:58 +0700

    fix bt ss7 UngDungMayTinhMoRong

--- ss7-bien_kieu_du_lieu_va_toan_tu/bai_tap/UngDungMayTinhMoRong.js
+++ ss7-bien_kieu_du_lieu_va_toan_tu/bai_tap/UngDungMayTinhMoRong.js
@@ -1,20 +1,20 @@
-
 let str = "";
-function clear() {
+
+function clean() {
     document.getElementById("input-box").value = "";
     str = "";
 }
 
 function addToOperator(e) {
-    document.getElementById("input-box").value;
     let btn_value = e.target.value;
     str += btn_value;
-    console.log(str);
     document.getElementById("input-box").value = str;
 
 }
+
 function showResult() {
     str = eval(str).toString()
     document.getElementById("input-box").value = str;
 }
-clear();
+
+clean();
