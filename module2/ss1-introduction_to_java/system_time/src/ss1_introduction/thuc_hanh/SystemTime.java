7b3c24937c5549fb9c792bba80d54f033998d32a
Nguyen <nguyen2697s@gmail.com>
2022-02-15 16:07:04 +0700

    add ss1-introduction.thuc_hanh

--- module2/ss1-introduction_to_java/system_time/src/ss1_introduction/thuc_hanh/SystemTime.java
+++ module2/ss1-introduction_to_java/system_time/src/ss1_introduction/thuc_hanh/SystemTime.java
@@ -1,3 +1,5 @@
+package ss1_introduction.thuc_hanh;
+
 import java.util.Date;
 
 public class SystemTime {
@@ -6,7 +8,6 @@ public class SystemTime {
     public static void main(String[] args) {
 
         Date now = new Date(); // Lấy ra thời gian hiện tại
-
         System.out.println("Now is: " + now);
     }
 }
